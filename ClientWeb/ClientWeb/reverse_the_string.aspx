18a2c32781da08d3885097981e4983070a9cc601
Wycliff <Wycliff@DESKTOP-FRA6SRU>
2018-06-20 12:57:56 +0300

    Initial

---
+++ ClientWeb/ClientWeb/reverse_the_string.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="reverse_the_string.aspx.cs" Inherits="codeChallangeWebClient.reverse_the_string" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="align-content:center">
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtValue" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Input:"></asp:Label>
+            <asp:Label ID="lblInput" runat="server" Text=""></asp:Label>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Output:"></asp:Label>
+            <asp:Label ID="lblOutput" runat="server" Text=""></asp:Label>
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ClientWeb/ClientWeb/reverse_the_string.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace codeChallangeWebClient
+{
+    public partial class reverse_the_string : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string text = txtValue.Text;
+            lblInput.Text = text;
+            char[] charArray = text.ToCharArray();
+            stringCharacter(charArray);
+            String reversedText = new String(charArray);
+
+            lblOutput.Text= reversedText;
+            txtValue.Text = string.Empty;
+        }
+
+        private void stringCharacter(char[] charArray)
+        {
+            //make an array of the string
+            char[] str1 = charArray;
+            //give value to the first and last value of the array
+            int a = str1.Length - 1;
+            int b = 0;
+            //Traverse string from the two ends
+
+            while (b < a)
+            {
+                //skip special characters
+                if (!char.IsLetter(str1[b]))
+                {
+                    b++;
+                }
+                else if (!char.IsLetter(str1[a]))
+                {
+                    a--;
+                }
+                else
+                {
+                    char str2 = str1[b];
+                    str1[b] = str1[a];
+                    str1[a] = str2;
+                    b++;
+                    a--;
+                }
+            }
+        }
+    }
+}
